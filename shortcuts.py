543d6c4c10ea7e4503642e105013b0f34d7a3173
WSHAPER <42714629+WSHAPER@users.noreply.github.com>
2025-12-06 15:22:44 +0100

    GH-6: fix(shortcuts): add missing Qt import

--- shortcuts.py
+++ shortcuts.py
@@ -1,4 +1,4 @@
-from PyQt6.QtCore import QObject, pyqtSignal
+from PyQt6.QtCore import QObject, pyqtSignal, Qt
 from PyQt6.QtGui import QKeySequence, QShortcut
 from PyQt6.QtWidgets import QApplication
 import logging
